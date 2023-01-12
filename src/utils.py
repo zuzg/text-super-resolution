0bd62405da9122392cf1e5f75b76dea5b826d856
azywot <agata.zywot@gmail.com>
2023-01-12 18:07:18 +0100

    relocate get_data function

--- src/utils.py
+++ src/utils.py
@@ -11,15 +11,3 @@ def imshow(a:np.ndarray):
         else:
             a = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
     display(PIL.Image.fromarray(a))
-
-# NOTE: we assume _img_HR.jpg and _img_HR.jpg suffices
-def get_data_from_dir(dir_path:str, filenames:list[str]) -> tuple[list[np.ndarray]]:
-    images_LR_HR = list()
-
-    for filename in filenames:
-        # add an exception in case of no filename file
-        imgLR = cv2.imread(dir_path+filename+'_img_LR.jpg', 1) 
-        imgHR = cv2.imread(dir_path+filename+'_img_HR.jpg', 1)
-        images_LR_HR.append((imgLR, imgHR))
-
-    return images_LR_HR
