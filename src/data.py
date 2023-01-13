05157b0d1bbcdf24d38036f7d8609f5a4f5f7484
azywot <agata.zywot@gmail.com>
2023-01-13 16:07:29 +0100

    add refactoring

--- src/data.py
+++ src/data.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from src.utils import imshow
+import matplotlib.pyplot as plt
 
 # NOTE: we assume _img_HR.jpg and _img_HR.jpg suffices
 def get_data_from_dir(dir_path:str, filenames:list[str], min_height:int=None) -> tuple[list[np.ndarray]]:
@@ -14,3 +16,23 @@ def get_data_from_dir(dir_path:str, filenames:list[str], min_height:int=None) ->
         images_LR_HR.append((imgLR, imgHR))
 
     return images_LR_HR
+
+def show_LR_HR_images(imgLR, imgHR): # move to src/data?
+    if imgHR.shape == imgLR.shape:
+        imshow(cv2.resize(np.concatenate([imgLR, imgHR], 1), None, fx=2, fy=2))
+    else:
+        imshow(cv2.resize(imgLR, None, fx=2, fy=2))
+        imshow(cv2.resize(imgHR, None, fx=2, fy=2))
+
+
+def get_height_width_distribution(shapes_list:list[tuple[int]]):
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+    height_list = list(map(lambda x: x[0], shapes_list))
+    width_list = list(map(lambda x: x[1], shapes_list))
+    # print(height_list[:3])
+    # print(width_list[:3])
+    axs[0].hist(height_list, color='deeppink')
+    axs[0].set_title("Images height distribution")
+    axs[1].hist(width_list, color='deeppink')
+    axs[1].set_title("Images width distribution")
+    plt.show()
--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import cv2
 import PIL
 import numpy as np
 from IPython.display import display
-
+import matplotlib.pyplot as plt
 
 def imshow(a: np.ndarray):
     a = a.clip(0, 255).astype('uint8')
@@ -12,3 +12,12 @@ def imshow(a: np.ndarray):
         else:
             a = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
     display(PIL.Image.fromarray(a))
+
+
+def display_img_tensor(img):
+    images_np  = img.cpu().numpy()
+    img_plt = images_np.transpose(1,2,0)
+    # print(img_plt.shape)
+    plt.grid(False)
+    plt.axis('off')
+    plt.imshow(np.clip(img_plt, 0, 1))
