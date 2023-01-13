9ccbbffc7af701193bfc31772c79ff89aee7f29f
azywot <agata.zywot@gmail.com>
2023-01-13 15:23:04 +0100

    train beta

--- src/data.py
+++ src/data.py
@@ -2,13 +2,15 @@ import cv2
 import numpy as np
 
 # NOTE: we assume _img_HR.jpg and _img_HR.jpg suffices
-def get_data_from_dir(dir_path:str, filenames:list[str]) -> tuple[list[np.ndarray]]:
+def get_data_from_dir(dir_path:str, filenames:list[str], min_height:int=None) -> tuple[list[np.ndarray]]:
     images_LR_HR = list()
 
     for filename in filenames:
         # add an exception in case of no filename file
         imgLR = cv2.imread(dir_path+filename+'_img_LR.jpg', 1) 
         imgHR = cv2.imread(dir_path+filename+'_img_HR.jpg', 1)
+        if min_height is not None and (imgLR.shape[0] < min_height):
+            continue
         images_LR_HR.append((imgLR, imgHR))
 
     return images_LR_HR
